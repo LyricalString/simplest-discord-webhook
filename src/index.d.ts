abf2835e4d7b251f917315fea183ebe8a3e6322e
eliyya <tecno_eli@hotmail.com>
2022-12-26 20:32:58 -0600

    add ts declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,16 @@
+import { AxiosResponse } from 'axios'
+import { MessageEmbed } from 'discord.js'
+export default interface webhook {
+    url: string
+    payload: { content: string } | { embeds: [MessageEmbed] }
+    templates: { [key: string]: MessageEmbed }
+    constructor(url: string): this
+    checkFieldValueLength(value: MessageEmbed): void
+    checkData(data: MessageEmbed | string): Promise<void>
+    preparePayload(data: MessageEmbed | string): Promise<{ content: string } | { embeds: [MessageEmbed] }>
+    createTemplate(identifier: string, templateData: MessageEmbed): Promise<void>
+    getTemplates(): Promise<MessageEmbed>
+    getSingleTemplate(identifier: string): Promise<MessageEmbed>
+    sendTemplate(template: string, options?: { title: string }): Promise<AxiosResponse>
+    send(data: MessageEmbed | string, template?: string): Promise<AxiosResponse>
+}
