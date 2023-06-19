f1074779dd05b2924e76e650b765934a3229859a
eliyya <tecno_eli@hotmail.com>
2023-06-19 13:28:23 -0600

    remove types

--- types/templates.d.ts
+++
@@ -1,7 +0,0 @@
-import { EmbedBuilder, Message, RESTPostAPIWebhookWithTokenJSONBody, WebhookMessageCreateOptions } from 'discord.js';
-declare const templates: Map<string, RESTPostAPIWebhookWithTokenJSONBody>;
-export type posiblePayload = string | EmbedBuilder | (WebhookMessageCreateOptions & RESTPostAPIWebhookWithTokenJSONBody) | Message;
-export type payload = {};
-export declare function createTemplate(identifier: string, templateData: posiblePayload): void;
-export declare function preparePayload(data: posiblePayload): RESTPostAPIWebhookWithTokenJSONBody;
-export default templates;
