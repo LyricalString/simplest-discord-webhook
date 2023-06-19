edb1453a048b777054672b98b7f092617d70a20f
eliyya <tecno_eli@hotmail.com>
2023-06-19 13:11:05 -0600

    remove dist

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,7 +3,7 @@ export { createTemplate };
 export default class Webhook {
     #private;
     constructor(url: string);
-    get templates(): Map<any, any>;
+    get templates(): Map<string, import("discord.js").RESTPostAPIWebhookWithTokenJSONBody>;
     get url(): string;
     static createTemplate: typeof createTemplate;
     sendTemplate(template: string, options: posiblePayload): Promise<any>;
--- types/templates.d.ts
+++ types/templates.d.ts
@@ -1,5 +1,5 @@
 import { EmbedBuilder, Message, RESTPostAPIWebhookWithTokenJSONBody, WebhookMessageCreateOptions } from 'discord.js';
-declare const templates: Map<any, any>;
+declare const templates: Map<string, RESTPostAPIWebhookWithTokenJSONBody>;
 export type posiblePayload = string | EmbedBuilder | (WebhookMessageCreateOptions & RESTPostAPIWebhookWithTokenJSONBody) | Message;
 export type payload = {};
 export declare function createTemplate(identifier: string, templateData: posiblePayload): void;
